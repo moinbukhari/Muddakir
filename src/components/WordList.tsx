2b646b5eb90b81cd336ae9633db378b7a3de9510
Moin Bukhari <moinbukhari4321@gmail.com>
2023-04-17 02:22:32 +0100

    removed use client

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { QuranicWord } from "./WordCard";
 import WordCard from "./WordCard";
 
--- src/components/WordSidebar.tsx
+++ src/components/WordSidebar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { QuranicWord } from "./WordCard";
 import { motion } from "framer-motion";
 import IconOnly from "./icon";
