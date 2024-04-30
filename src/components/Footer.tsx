9186b819469558f205d863f6263ccf57a823de48
moinbukhari <moinbukhari4321@gmail.com>
2024-04-30 11:30:02 +0100

    update twitter footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,26 @@
 export const Footer = () => {
   return (
-    <footer className="md:px-4 py-3">
+    <footer className="py-3 md:px-4">
       <div className="container flex flex-col items-center justify-between gap-4 md:flex-row">
-        <p className="text-center text-sm md:text-base leading-loose md:text-left">
+        <p className="text-center text-sm leading-loose md:text-left md:text-base">
           Built by{" "}
           <a
             href="https://twitter.com/Moin4321"
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline text-slate-700 underline-offset-4 hover:text-slate-950"
+            className="font-medium text-slate-700 underline underline-offset-4 hover:text-slate-950"
           >
             moin
           </a>
           . Any bugs or feature requests send{" "}
           <a
-            href="https://twitter.com/Moin4321"
+            href="https://twitter.com/moinbukh"
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline text-slate-700 underline-offset-4 hover:text-slate-950"
+            className="font-medium text-slate-700 underline underline-offset-4 hover:text-slate-950"
           >
             here
-          </a> 
+          </a>
         </p>
       </div>
     </footer>
